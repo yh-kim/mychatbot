e5e733ef6bdf56bc6fbaad182b3cd6a55428efc3
yh-kim <btc_yh@naver.com>
2017-06-25 23:31:06 +0900

    Add floating view animator

--- app/src/main/java/com/pickth/mychatbot/service/FloatingViewService.kt
+++ app/src/main/java/com/pickth/mychatbot/service/FloatingViewService.kt
@@ -4,7 +4,9 @@ import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.graphics.PixelFormat
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
 import android.view.*
 import com.pickth.mychatbot.R
 import com.pickth.mychatbot.util.Dlog
@@ -23,8 +25,18 @@ class FloatingViewService: Service() {
     private lateinit var mFloatingView: View
     var initialX: Int = 0
     var initialY: Int = 0
-    var initialTouchX: Float = 0.0f
-    var initialTouchY: Float = 0.0f
+    var touchX: Float = 0.0f
+    var touchY: Float = 0.0f
+    var params = WindowManager.LayoutParams(
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.TYPE_PHONE,
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+            PixelFormat.TRANSLUCENT
+    )
+    var isBeingDrag = false
+    var moveAnimator = MoveAnimator()
+
 
     override fun onBind(p0: Intent?): IBinder? {
         return null
@@ -37,13 +49,7 @@ class FloatingViewService: Service() {
         mFloatingView = LayoutInflater.from(this).inflate(R.layout.floating_widget, null)
 
         // Add the view to the window
-        var params = WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_PHONE,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.TRANSLUCENT
-        )
+
 
         // Specify the view position
         params.gravity = Gravity.TOP or Gravity.LEFT
@@ -62,37 +68,36 @@ class FloatingViewService: Service() {
                         initialX = params.x
                         initialY = params.y
 
-                        initialTouchX = motionEvent.rawX
-                        initialTouchY = motionEvent.rawY
+                        touchX = motionEvent.rawX
+                        touchY = motionEvent.rawY
+
+                        moveAnimator.stop()
+                        isBeingDrag = true
                         true
                     }
                     MotionEvent.ACTION_MOVE -> {
-                        params.x = initialX + (motionEvent.rawX - initialTouchX).toInt()
-                        params.y = initialY + (motionEvent.rawY - initialTouchY).toInt()
-                        Dlog.v("params x:${params.x}, y:${params.y}, initialX:${initialX} , initialY:${initialY} , \t${motionEvent.rawX}\t${motionEvent.rawY}\t${initialTouchX}\t${initialTouchY}")
+                        if(isBeingDrag) {
+//                            move(initialX + (motionEvent.rawX - touchX), initialY + (motionEvent.rawY - touchY))
+                            move((motionEvent.rawX - touchX), (motionEvent.rawY - touchY))
+                        }
+                        touchX = motionEvent.rawX
+                        touchY = motionEvent.rawY
 
-                        // Update the layout with new X & Y coordinate
-                        mWindowManager.updateViewLayout(mFloatingView, params)
                         true
                     }
                     MotionEvent.ACTION_UP -> {
-                        val Xdiff = (motionEvent.rawX - initialTouchX).toInt()
-                        val Ydiff = (motionEvent.rawY - initialTouchY).toInt()
+//                        val Xdiff = (motionEvent.rawX - touchX).toInt()
+//                        val Ydiff = (motionEvent.rawY - touchY).toInt()
+                        val Xdiff = params.x - initialX
+                        val Ydiff = params.y - initialY
                         if(Xdiff < 10 && Ydiff < 10) {
                             // click event
                         }
+
                         Dlog.v("param.x: ${params.x} , defaultDisplay.width: ${mWindowManager.defaultDisplay.width}, width: ${mFloatingView.width}")
+                        isBeingDrag = false
+                        goToSide()
 
-                        if(params.x < mWindowManager.defaultDisplay.width/2 - mFloatingView.width/2 ) {
-                            params.x = 0
-                            Dlog.v("Move button to left")
-                        } else {
-                            params.x = mWindowManager.defaultDisplay.width - mFloatingView.width
-                            Dlog.v("Move button to right")
-                        }
-
-                        mWindowManager.updateViewLayout(mFloatingView, params)
-                        Dlog.v("x: ${params.x}, y: ${params.y}")
                         true
                     }
                     else -> false
@@ -102,10 +107,69 @@ class FloatingViewService: Service() {
         }
     }
 
+    fun move(x:Float, y:Float) {
+        params.x += x.toInt()
+        params.y += y.toInt()
+        Dlog.v("params x:${params.x}, y:${params.y}, initialX:${initialX} , initialY:${initialY} \t${touchX}\t${touchY}")
+
+        // Update the layout with new X & Y coordinate
+        mWindowManager.updateViewLayout(mFloatingView, params)
+    }
+
+    fun goToSide() {
+        if(params.x < mWindowManager.defaultDisplay.width/2 - mFloatingView.width/2 ) {
+            moveAnimator.setPosition(0f, params.y.toFloat())
+            Dlog.v("Move button to left")
+
+        } else {
+            moveAnimator.setPosition((mWindowManager.defaultDisplay.width - mFloatingView.width).toFloat(), params.y.toFloat())
+            Dlog.v("Move button to right")
+
+        }
+        Dlog.v("x: ${params.x}, y: ${params.y}")
+    }
+
+    /*
+    fun View.moveItem(x1:Int, x2: Int, y1: Int, y2: Int) {
+        val animation = TranslateAnimation(x1.toFloat(), x2.toFloat(), y1.toFloat(), y2.toFloat())
+        animation.duration = 10000
+        Dlog.v("Start animation")
+        startAnimation(animation)
+    }
+    */
 
     override fun onDestroy() {
         super.onDestroy()
         if(mFloatingView != null) mWindowManager.removeView(mFloatingView)
         Dlog.v("서비스 종료")
+    }
+
+    inner class MoveAnimator: Runnable {
+        // get main thread looper
+        private val handler = Handler(Looper.getMainLooper())
+        private var destinationX: Float = 0f
+        private var destinationY: Float = 0f
+        var startingTime: Long = 0
+
+        fun setPosition(x: Float, y: Float) {
+            this.destinationX = x
+            this.destinationY = y
+            startingTime = System.currentTimeMillis();
+            handler.post(this)
+        }
+
+        override fun run() {
+            val progress = Math.min(1f, (System.currentTimeMillis() - startingTime) / 400f)
+            var deltaX = (destinationX - params.x) * progress
+            var deltaY = (destinationY - params.y) * progress
+            move(deltaX, deltaY)
+            if(progress < 1) {
+                handler.post(this)
+            }
+        }
+
+        fun stop() {
+            handler.removeCallbacks(this)
+        }
     }
 }
